c6585cce2c92fb29a81b3dad43b31251839fd8be
Joseph <josephiusx@gmail.com>
2021-04-14 14:41:11 -0700

    20. Navigation bar and styling

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 export default function App() {
   return (
     <div>
-      <h1>Re-vents</h1>
-    <EventDashboard />
+      <NavBar />
+      <Container className='main'>
+      <EventDashboard />
+      </Container>
     </div>
   );
 }
+
 
 
