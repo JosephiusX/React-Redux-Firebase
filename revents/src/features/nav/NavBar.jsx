b02691d4cb67fffa96c9f44540cebc075df5c8cd
Joseph <josephiusx@gmail.com>
2021-04-15 12:03:14 -0700

    24. React component state. end of section 3

--- revents/src/features/nav/NavBar.jsx
+++ revents/src/features/nav/NavBar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Menu, Container, Button} from 'semantic-ui-react';
 
-export default function NavBar() {
+export default function NavBar({setFormOpen}) {
     return (
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item>
+                <Menu.Item header>
                     <img src = '/assets/logo.png' alt='logo' style={{marginRight: 15}}/>
                     Re-vents
                 </Menu.Item>
                 <Menu.Item name='Events' />
                 <Menu.Item>
-                    <Button positive inverted content = 'Create Event' />
+                    <Button onClick={() => setFormOpen(true)} positive inverted content = 'Create Event' />
                 </Menu.Item>
                 <Menu.Item position='right'>
                     <Button basic inverted content = 'Login' />
@@ -20,4 +20,6 @@ export default function NavBar() {
             </Container>
         </Menu>
     )
-}
+}   
+
+// const [formOpen, setFormOpen] = useState(false);
